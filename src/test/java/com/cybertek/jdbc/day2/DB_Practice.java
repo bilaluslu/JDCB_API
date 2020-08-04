41ddbf25af6f296116d18259465d56bafc39c5c6
bilaluslu <uslubilal45@gmail.com>
2020-08-04 11:29:12 -0400

    day2

--- src/test/java/com/cybertek/jdbc/day2/DB_Practice.java
+++ src/test/java/com/cybertek/jdbc/day2/DB_Practice.java
@@ -1,6 +1,18 @@
 package com.cybertek.jdbc.day2;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DB_Practice {
+
+    public static void main(String[] args) throws SQLException {
+        // print out all data from Jobs Table
+        DB_Utility.createConnection();
+        ResultSet rs = DB_Utility.runQuery("SELECT * FROM JOBS");
+        // ITERATE OVER THE RESULTSET
+        rs.next();
+        System.out.println(  );
+    }
 
 
 }
